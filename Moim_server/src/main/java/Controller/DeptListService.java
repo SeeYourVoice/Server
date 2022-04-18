f9d0c486488d4e3dae53aa46406b131bf9a00e08
nohmiran <nmr1227@gmail.com>
2022-04-18 20:56:36 +0900

    4월18일 회원가입 완료, 부서 리스트 수정 중

--- Moim_server/src/main/java/Controller/DeptListService.java
+++ Moim_server/src/main/java/Controller/DeptListService.java
@@ -11,21 +11,41 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import model.DeptDAO;
+
 @WebServlet("/Moim_DeptListService")
 public class DeptListService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html; charset=utf-8");
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("application/json; charset=utf-8");
 		PrintWriter out = response.getWriter();
-		
+
+		JsonObject jsonObj = new JsonObject();
+		JsonArray jsonArr = new JsonArray();
+
 		ArrayList<String> dept_name = new ArrayList<String>();
-		
+
 		// 부서 리스트
-		request.getParameter("dept_name");
-			
-			
-			
+		DeptDAO dao = new DeptDAO();
+
+		dept_name = dao.deptdto();
+
+		for (String dept : dept_name) {
+//			System.out.println(dept);
+			jsonArr.add(dept);
+		}
+
+		jsonObj.add("dept_list", jsonArr);
+
+		System.out.println(jsonObj.toString());
+
+		out.print(jsonObj.toString());
+
 	}
 
 }
--- Moim_server/src/main/java/Controller/LoginService.java
+++ Moim_server/src/main/java/Controller/LoginService.java
@@ -12,67 +12,56 @@ import javax.servlet.http.HttpSession;
 
 import com.google.gson.JsonObject;
 
-
 import model.UserDAO;
 import model.UserDTO;
-
 
 @WebServlet("/Moim_LoginService")
 public class LoginService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.setContentType("text/html; charset=utf-8");
-		PrintWriter out = response.getWriter();		
-		//Gson gson = new Gson();
-			
-		 //=========로그인정보=========
+		PrintWriter out = response.getWriter();
+		// Gson gson = new Gson();
+
+		// =========로그인정보=========
 		System.out.println("========= 로그인 서비스  =========");
 
-		
-		String id=request.getParameter("id");
-		String pw=request.getParameter("pw");
-		
-		//DB에서 일치하는지 보고                                                
-		UserDAO userDAO=new UserDAO();
-		UserDTO userDTO=new UserDTO(id, pw); 
-		
-		//회원가입 여부 판단
-		UserDTO info=userDAO.LoginDB(userDTO);
-		 
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
 
-		String jsonStr="false";
-		JsonObject obj=new JsonObject();
-		
-			
-		if(info!=null) {//로그인 성공
+		// DB에서 일치하는지 보고
+		UserDAO userDAO = new UserDAO();
+		UserDTO userDTO = new UserDTO(id, pw);
+
+		// 회원가입 여부 판단
+		UserDTO info = userDAO.LoginDB(userDTO);
+
+		String jsonStr = "false";
+		JsonObject obj = new JsonObject();
+
+		if (info != null) {// 로그인 성공
 			System.out.println("로그인 성공");
-			
+
 			obj.addProperty("user_email", info.getUser_email());
-			obj.addProperty("user_password",info.getUser_password());
+			obj.addProperty("user_password", info.getUser_password());
 			obj.addProperty("first_name", info.getFirst_name());
 			obj.addProperty("last_name", info.getLast_name());
-			obj.addProperty("profile_img", info.getProfile_img()); //주소값을 넘겨줌.
+			obj.addProperty("profile_img", info.getProfile_img()); // 주소값을 넘겨줌.
 			obj.addProperty("user_joindate", info.getUser_joindate());
 			obj.addProperty("corp_name", info.getCorp_name());
 			obj.addProperty("dept_seq", info.getDept_seq());
-			obj.addProperty("dept",info.getDept_name());
+			obj.addProperty("dept", info.getDept_name());
 			obj.addProperty("position_num", info.getPosition_num());
 			obj.addProperty("position_name", info.getPosition_name());
-			
-			//안드로이드 전달
+
+			// 안드로이드 전달
 			out.print(obj.toString());
-			
-		}else {//로그인 실패
+
+		} else {// 로그인 실패
 			out.print(jsonStr);
 		}
-		
-		
-		
-
-		 
-		 
-
 
 	}
 
